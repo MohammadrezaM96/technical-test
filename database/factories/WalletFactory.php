60bda7a9b0a358437f9a50ef8ce12ad2b0178bf7
mohammadrezam <mardani93.itsu@gmail.com>
2022-08-03 19:08:07 +0430

    feat: Added Wallet Factory

---
+++ database/factories/WalletFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Wallet>
+ */
+class WalletFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::query()->first()->id,
+            'address' => bcrypt('textTest'),
+            'balance' => 8937834823428923468324565345435000000000000000000000000000000000,
+        ];
+    }
+}
