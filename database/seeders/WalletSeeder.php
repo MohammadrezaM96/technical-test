2c1d8c5020f23c3e1b00a94ab89cedd66d5d79bb
mohammadrezam <mardani93.itsu@gmail.com>
2022-08-03 19:08:29 +0430

    feat: Added Wallet Seeder

---
+++ database/seeders/WalletSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Currency;
+use App\Models\Wallet;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class WalletSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Wallet::factory(1)->create();
+    }
+}
